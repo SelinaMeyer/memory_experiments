ec7f47cab810e841913a2b097c0e54702046d670
Selina Meyer <selina.meyer@utn.de>
2025-05-08 16:46:55 +0200

    updated distractor phase to be less textbased

--- memory_experiment/pages/distractor_page.py
+++ memory_experiment/pages/distractor_page.py
@@ -1,35 +1,65 @@
 import streamlit as st
 import time
 
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 if st.session_state.get("distractor_timer") not in st.session_state:
         st.session_state.distractor_timer = time.time()
 
 @st.fragment(run_every=1)
 def distraction():
-    if time.time() - st.session_state.get("distractor_timer") < 10:
-        st.radio(label="Would you rather live for the rest of your life in the Arctic or in the Sahara desert?",
-            options=["Arctic", "Sahara desert"], index=None, key="distractor_radio")
-    elif time.time() - st.session_state.get("distractor_timer") >= 10 and time.time() - st.session_state.get("distractor_timer") < 20:
-        st.radio(label="If you could only eat one food for the rest of your life, what would it be?",
-            options=["Pizza", "Sushi", "Salad", "Chocolate"], index=None, key="distractor_radio1")
-    elif time.time() - st.session_state.get("distractor_timer") >= 20 and time.time() - st.session_state.get("distractor_timer") < 30:
-        st.radio(label="If age is only a state of mind, which category best describes your state of mind right now?",
-            options=["Cheeky child", "Tormented teenager", "Mad mid-lifer", "Groovy grandparent"], index=None, key="distractor_radio2")
+    if time.time() - st.session_state.get("distractor_timer") < 15:
+        st.image("memory_experiment/resources/cat_scale.webp", width=500)
+        st.segmented_control(label="scale",
+            options=[1,2,3,4,5,6,7,8,9], label_visibility="hidden", key="distractor_radio")
+    elif time.time() - st.session_state.get("distractor_timer") >= 15 and time.time() - st.session_state.get("distractor_timer") < 30:
+        st.write("Where would you rather live for the rest of your life?")
+        st.image("memory_experiment/resources/landscapes.png", width=500)
+        st.segmented_control(label="Where would you rather live for the rest of your life?",
+            options=[1,2,3,4], label_visibility="hidden", key="distractor_radio2")
     elif time.time() - st.session_state.get("distractor_timer") >= 30 and time.time() - st.session_state.get("distractor_timer") < 45:
-        st.radio(label="If your city were a breed of dog, which breed would it be?",
-             options=["Jack Russell - small, tough, opinionated",
-              "Tibetan Mastiff - or some other very rare breed",
-              "German Shepherd - poised and elegant, but rather hardy",
-              "Poodle - beautifully presented, but a bit of a poser",
-              "Golden Retriever - warm, cuddly and great with children",
-              "Pit Bull Terrier - scary, but kind deep down",
-              "Labradoodle - or some other cute hybrid"], index=None, key="distractor_radio3")
+        st.write("What do you see in the image?")
+        st.image("memory_experiment/resources/hammerhead_shark.webp", width=500)
+        st.radio(label="What do you see in the image?",
+             options=["A \"hammerhead shark\"",
+              "A \"humpback whale\"",
+              "A \"bottlenose dolphin\""], label_visibility="hidden", index=None, key="distractor_radio3")
     elif time.time() - st.session_state.get("distractor_timer") >= 45 and time.time() - st.session_state.get("distractor_timer") < 60:
-        st.radio(label="If you could be any animal, which one would you be?",
-            options=["A lion - king of the jungle",
-                     "A dolphin - intelligent and playful",
-                     "An eagle - free to fly wherever I want",
-                     "A tortoise - slow and steady wins the race"], index=None, key="distractor_radio4")
+        st.write("What is your ideal night out?")
+        st.image("memory_experiment/resources/night_out.png", width=500)
+        st.segmented_control(label="What's your ideal night out?",
+            options=[1,2,3,4],label_visibility="hidden", key="distractor_radio4")
     elif time.time() - st.session_state.get("distractor_timer") >= 60:
         st.switch_page("memory_experiment/pages/recall_page.py")
 
