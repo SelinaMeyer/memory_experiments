56476012f411f7875e2f9cbc6e84c93585935f37
Selina Meyer <selina.meyer@utn.de>
2025-05-12 20:54:24 +0200

    updated informed consent and headlines

--- memory_experiment/pages/informed_consent_page.py
+++ memory_experiment/pages/informed_consent_page.py
@@ -48,17 +48,16 @@ if not user:
             st.html("<h1>Welcome to this Experiment!")
             st.html("""
 <h1>Informed Consent of Participation 
-<p>You are invited to participate in the online study "Memorability And Credibility Of News Headlines" initiated and conducted by Dr. Selina Meyer. The research is supervised by Prof. Dr. Michael Roth and Prof. Dr. Magdalena Abel at the UTN Nuremberg. Please note: Your participation is enitrely voluntary.
-                    <br>The online study will last approximately 20 minutes. 
-                    <br>If you have any questions or complaints about the informed consent process of this research study or your rights as a human research subject, please contact Dr. Selina Meyer (E-Mail: selina.meyer@utn.de) or Prof. Dr. Michael Roth & Prof. Dr. Magdalena Abel. 
-                    <br>You should carefully read the information below. Please take as much time as you need to read the consent form. 
+<p>You are invited to participate in the online study "Memorability And Credibility Of News Headlines" initiated and conducted by Dr. Selina Meyer. The research is supervised by Prof. Dr. Michael Roth and Prof. Dr. Magdalena Abel at the UTN Nuremberg. 
+                    <br>If you have any questions or complaints about the informed consent process of this research study or your rights as a human research subject, please contact Dr. Selina Meyer (E-Mail: selina.meyer@utn.de). 
+                    <br>Please carefully read the information below.
 <h2>1. Purpose and Goal of this Research
 <p>The purpose of the research project is to identify the dynamics at play in the proliferation of information online. 
                     The goal of this study is to explore to which extent certain linguistic features affect memorability and truthfulness judgements of news headlines. 
                     <br>Your participation will help us achieve this goal. The results of this research and collected data may be presented at scientific or professional meetings or published in scientific proceedings and journals. 
 <h2>2. Participation and Compensation
-<p>Your participation in this online study is completely voluntary. You will be one of approximately 60 people being surveyed for this research. You will receive 4 GBP as compensation for your participation. 
-                    <br>You may withdraw and discontinue participation at any time without penalty or losing the compensation. You may refuse to answer any questions about yourself you do not want to answer. 
+<p>Your participation in this online study is completely voluntary. You will be one of approximately 120 people being surveyed for this research. You will receive 4 GBP as compensation for your participation. 
+                    <br>You may withdraw and discontinue participation at any time without penalty or losing the compensation. 
 <h2>3. Procedure
 <p>In this study, your initial task will be to read and memorize news headlines. 
                     You will then be asked to remember the headlines and to subjectively judge and rate the headlines. 
@@ -67,7 +66,7 @@ if not user:
 <h2>4. Risks and Benefits
 <p>There are no risks associated with this online study. Discomforts or inconveniences will be minor and are not likely to happen. 
 <h2>5. Data Protection and Confidentiality
-<p>Some personal data (gender, political affiliation, etc.) will be recorded while participation. You have the option not to answer any demographic questions you do not want to answer.
+<p>Some personal data (gender, political affiliation, etc.) will be recorded during participation. You may refuse to answer any questions about yourself you do not want to answer. 
                     No personally identifiable information such as your name, address, or email address will be recorded.
                     All data you provide in this online study will be published anonymized and treated confidentially in compliance with the General Data Protection Regulation (GDPR) of the European Union (EU). 
                     Subsequent uses of records and data will be subject to standard data use policies which protect the full anonymity of the participating individuals. 
