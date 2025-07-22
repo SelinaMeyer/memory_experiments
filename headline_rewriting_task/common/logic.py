13d19dc0d9a8de3c45d5fbbe8bd25c2a6856f00e
Selina Meyer <selina.meyer@utn.de>
2025-07-22 14:17:11 +0200

    hrug

--- headline_rewriting_task/common/logic.py
+++ headline_rewriting_task/common/logic.py
@@ -14,9 +14,9 @@ def check_if_qualified(annotations: dict) -> bool:
     accuracy_score = 0
     style_score = 0
     for question_id in qualification_questions:
-        if annotations["qualification"][int(question_id)]["accuracy"] == qualification_questions[question_id]["correct_accuracy_answer"]:
+        if annotations["qualification"][int(question_id)-1]["accuracy"] == qualification_questions[question_id]["correct_accuracy_answer"]:
             accuracy_score += 1
-        if annotations["qualification"][int(question_id)]["style"] == qualification_questions[question_id]["correct_style_answer"]:
+        if annotations["qualification"][int(question_id)-1]["style"] == qualification_questions[question_id]["correct_style_answer"]:
             style_score += 1
     
     if (accuracy_score >= needed_accuracy_score and style_score >= needed_style_score):
