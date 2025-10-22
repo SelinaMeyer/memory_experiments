185236cfd3ae33d64e9495da373f19e063316f07
Selina Meyer <selina.meyer@utn.de>
2025-10-22 10:14:40 +0200

    testing

--- change_detection_task/pages/Experiment_description_page.py
+++ change_detection_task/pages/Experiment_description_page.py
@@ -38,4 +38,4 @@ st.html("""<p>Im Folgenden siehst du 16 englischsprachige Nachrichtenüberschrif
         <p>Drücke auf den Button unten, um mit dem Experiment zu beginnen.""")
 
 if st.button("Ich bin bereit", key="start_experiment_button"):
-    st.switch_page("change_detection_task/pages/presentation_page.py")
+    st.switch_page("change_detection_task/pages/distractor_page.py")
