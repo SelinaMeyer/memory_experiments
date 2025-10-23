272475d8e0b96b960c1bcc330522d555a7d4efbe
Selina Meyer <selina.meyer@utn.de>
2025-10-23 11:42:05 +0200

    changed bar chart

--- change_detection_task/pages/thank_you_page.py
+++ change_detection_task/pages/thank_you_page.py
@@ -21,24 +21,29 @@ time.sleep(0.5)
 st.write("Wie hat die KI im Vergleich abgeschnitten?")
 with st.spinner():
     time.sleep(2)
-st.bar_chart(dat_df, stack=False, horizontal=True)
+st.bar_chart(dat_df, stack=False, horizontal=True, x_label="Anzahl richtig eingeordneter Überschriften", y_label="", y=[0,1,2,3,4,5,6,7], x=["Du", "KI"])
 time.sleep(0.5)
 
 if st.session_state.correct_answer_count == 6:  
+    st.write("Das Sprachmodell hat 6 Überschriften richtig eingeordnet.")
     st.html("<h3>Glückwunsch! Dein Gedächtnis ist genauso gut, wie das unseres Sprachmodells!</h3>")
+    st.balloons()
     st.balloons()
 elif st.session_state.correct_answer_count > 6:
     st.balloons()
-    st.html("<h3>Wow! Dein Gedächtnis ist sogar besser als das unseres Sprachmodells!</h3>")
-    st.write("Das Sprachmodell hat nur 6 Überschriften richtig eingeordnet.")
-else:
-    st.html("<h3>Unser Sprachmodell hat mehr Überschriften richtig eingeordnet als du. Aber keine Sorge, das ist ganz normal!</h3>")
+    st.balloons()
     st.write("Das Sprachmodell hat 6 Überschriften richtig eingeordnet.")
+    st.html("<h3>Wow! Dein Gedächtnis ist sogar besser als das unseres Sprachmodells!</h3>")
+else:
+    st.write("Das Sprachmodell hat 6 Überschriften richtig eingeordnet.")
+    st.html("<h3>Unser Sprachmodell hat mehr Überschriften richtig eingeordnet als du. Aber keine Sorge, das ist ganz normal!</h3>")
 
         
 with st.container(border=False, key="my_blue_container"):
-    st.html("<p>Als KI haben wir Llama3 8b verwendet.</p><p>Dabei handelt es sich um ein relativ kleines LLM, das man auch lokal auf gängigen Computern laufen lassen kann.</p>"
-        "<p>Anstatt der Bilder, die du gesehen hast, haben wir Llama zwischen der Präsentation der Headlines und dem Gedächtnistest 4000 Zeichen einer Sherlock Holmes geschichte übergeben.</p>"
+    st.html("<i>Ein paar zusätzliche Details:<i>")
+    st.html("<p>Als KI haben wir Llama3 8b verwendet. Dabei handelt es sich um ein relativ kleines LLM, das man auch lokal auf gängigen Computern laufen lassen kann.</p>"
+        "<p>Anstatt der Bilder, die du gesehen hast, haben wir Llama zwischen der Präsentation der Headlines und dem Gedächtnistest 4000 Zeichen einer Sherlock Holmes geschichte übergeben.</p>")
+    st.html(
         "<p>Die Nachrichtenüberschriften wurden aus dem Englischen übersetzt.</p><p>Alle Änderungen an Headlines wurden durch verschiedene LLMs durchgeführt.</p>" \
         "<p>Wir forschen daran, wie wir diese Techniken nutzen können um wahre Nachrichten so zu ändern, das sie besser in Erinnerung bleiben, ohen dabei die Kernaussage zu verändern.</p>" \
         "<p>Damit möchten wir den Einfluss von Misinformation und Fake News reduzieren.</p>")
