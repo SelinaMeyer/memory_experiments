0d7a6c272e7b03404921d237dff0aad02e5cbef8
Selina Meyer <selina.meyer@utn.de>
2025-10-23 11:54:57 +0200

    test

--- change_detection_task/pages/thank_you_page.py
+++ change_detection_task/pages/thank_you_page.py
@@ -22,7 +22,7 @@ time.sleep(0.5)
 st.write("Wie hat die KI im Vergleich abgeschnitten?")
 with st.spinner():
     time.sleep(2)
-st.bar_chart(dat_df, stack=False, horizontal=True, x_label="Anzahl der richtig eingeordneten Überschriften", y_label="")
+st.bar_chart(dat_df, stack=False, horizontal=True, x_label="Anzahl der richtig eingeordneten Überschriften", y_label=" ", x=[0,1,2,3,4,5,6,7,8])
 time.sleep(0.5)
 
 if st.session_state.correct_answer_count == 6:  
