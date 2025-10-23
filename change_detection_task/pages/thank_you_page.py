0a85975adc0b97a9d172be58d494e8946871391b
Selina Meyer <selina.meyer@utn.de>
2025-10-23 11:48:05 +0200

    bar chart stuff

--- change_detection_task/pages/thank_you_page.py
+++ change_detection_task/pages/thank_you_page.py
@@ -21,7 +21,7 @@ time.sleep(0.5)
 st.write("Wie hat die KI im Vergleich abgeschnitten?")
 with st.spinner():
     time.sleep(2)
-st.bar_chart(dat_df, stack=False, horizontal=True, x=["Du", "KI"], y="", y_label=[0,1,2,3,4,5,6,7])
+st.bar_chart(dat_df, stack=False, horizontal=True, y=["Du", "KI"])
 time.sleep(0.5)
 
 if st.session_state.correct_answer_count == 6:  
